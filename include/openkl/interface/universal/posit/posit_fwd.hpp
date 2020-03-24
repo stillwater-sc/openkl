28e8543ba32ac87176c72d209540db67e0a80cce
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-24 14:02:10 +0100

    Rearranged declarations.

---
+++ include/openkl/interface/universal/posit/posit_fwd.hpp
@@ -0,0 +1,18 @@
+// posit_fwd.hpp
+// Created: 2020-03-24
+//
+// Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
+//
+// This file is part of the OpenKL project, which is released under an MIT Open Source license.
+// Authors: Peter Gottschling (peter.gottschling@simunova.com)
+//          Theodore Omtzigt (theo@stillwater-sc.com)
+
+#pragma once
+
+#include <cstddef>
+
+namespace sw { namespace unum {
+
+template <std::size_t NBits, std::size_t Es> class posit;
+
+}} // namespace sw::unum
---
+++ include/openkl/openkl_fwd.hpp
@@ -0,0 +1,21 @@
+// openkl_fwd.hpp
+// Created: 2020-03-24
+//
+// Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
+//
+// This file is part of the OpenKL project, which is released under an MIT Open Source license.
+// Authors: Peter Gottschling (peter.gottschling@simunova.com)
+//          Theodore Omtzigt (theo@stillwater-sc.com)
+
+#pragma once
+
+#include <openkl/interface/universal/posit/posit_fwd.hpp>
+
+namespace openkl {
+
+using posit32= sw::unum::posit<32, 2>; // or something else   
+
+class object_id; 
+
+
+} // namespace openkl
