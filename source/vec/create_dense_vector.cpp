2ddd53943b2a1ebee13aede76e84202cc96038b2
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-24 17:43:18 +0100

    Have compiled library now.

---
+++ source/vec/create_dense_vector.cpp
@@ -0,0 +1,27 @@
+// create_dense_vector.cpp
+// Created: 2020-03-24
+//
+// Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
+//
+// This file is part of the OpenKL project, which is released under an MIT Open Source license.
+// Authors: Peter Gottschling (peter.gottschling@simunova.com)
+//          Theodore Omtzigt (theo@stillwater-sc.com)
+
+#include <openkl/openkl_fwd.hpp>
+#include <openkl/mtl5_shim.hpp>
+
+#include <openkl/utilities/object_id.hpp>
+
+
+
+namespace openkl {
+
+    
+object_id create_dense_vector(size_t s, const posit32& data)
+{
+    return object_id{};
+}    
+    
+
+
+} // namespace openkl
