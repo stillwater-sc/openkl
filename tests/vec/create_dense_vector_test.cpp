29cf43b9dad7bb3e9e0d3ebf3f383aaa08a7d055
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-24 16:49:30 +0100

    Started with tests.

--- tests/vec/create_dense_vector_test.cpp
+++ tests/vec/create_dense_vector_test.cpp
@@ -1,6 +1,5 @@
-{% load kdev_filters %}
-// {{ name }}.cpp
-// Created: {% now "yyyy-MM-dd" %}
+// create_dense_vector_test.cpp
+// Created: 2020-03-24
 //
 // Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
 //
@@ -8,40 +7,27 @@
 // Authors: Peter Gottschling (peter.gottschling@simunova.com)
 //          Theodore Omtzigt (theo@stillwater-sc.com)
 
-
-
 #include <iostream>
-
-
 
 #include <openkl/openkl_fwd.hpp>
 #include <openkl/mtl5_shim.hpp>
-
-
+// #include <openkl/vec/create_dense_vector.hpp>
 
 #include <universal/posit/posit.hpp>
 
-
-
+#include <mtl/vec/dense_vector.hpp>
 #include <mtl/operations/io/test_ostream.hpp>
 #include <mtl/testing/check_equal.hpp>
 
-
-
-using openkl::io::tout;
-using openkl::check_equal;
-
-
-
+using mtl::io::tout;
+using mtl::check_equal;
 
 int main()
 {
-   
+    using namespace openkl;
     
-    
+    mtl::vec::dense_vector<posit32> v{2.3, 4.1, 6.1};
+    // create_dense_vector(size(v), &v[0]);
 
-    
     return 0;
 }
-
-
