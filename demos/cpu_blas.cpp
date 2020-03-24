7e219190e5ed41ef68ad6f1e41782c4ccdb6c2f7
Theodore Omtzigt <theodore.omtzigt@ibm.com>
2020-03-24 09:45:55 -0400

    first draft of a device database and execution context

---
+++ demos/cpu_blas.cpp
@@ -0,0 +1,10 @@
+// cpu_blas.cpp: example of BLAS operators using CPU
+//
+#include <iostream>
+
+int main(int argc, char* argv[])
+try {
+}
+catch(const char* msg) {
+	std::cerr << "caught exception: " << msg << std::endl;
+}
