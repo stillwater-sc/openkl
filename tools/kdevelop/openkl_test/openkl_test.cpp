ed662fc14b991a68427e62905ebbaa838ace96bb
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-24 11:33:17 +0100

    KDevelop template for tests.

---
+++ tools/kdevelop/openkl_test/openkl_test.cpp
@@ -0,0 +1,40 @@
+// {{ name }}.cpp
+// Created: {% now "yyyy-MM-dd" %}
+//
+// Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
+//
+// This file is part of the OpenKL project, which is released under an MIT Open Source license.
+// Authors: Peter Gottschling (peter.gottschling@simunova.com)
+//          Theodore Omtzigt (theo@stillwater-sc.com)
+
+
+
+#include <iostream>
+
+
+
+#include <openkl/operations/io/test_ostream.hpp>
+
+
+
+#include <openkl/testing/check_equal.hpp>
+
+
+
+using openkl::io::tout;
+using openkl::check_equal;
+
+
+
+
+int main()
+{
+   
+    
+    
+
+    
+    return 0;
+}
+
+
