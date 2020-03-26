1893fece0cb948f3f7d93cdebf71537424bf432d
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-26 13:05:28 +0100

    Test for write_dense_matrix.

---
+++ source/mat/write_dense_matrix.cpp
@@ -0,0 +1,19 @@
+// write_dense_matrix.cpp
+// Created: 2020-03-26
+//
+// Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
+//
+// This file is part of the OpenKL project, which is released under an MIT Open Source license.
+// Authors: Peter Gottschling (peter.gottschling@simunova.com)
+//          Theodore Omtzigt (theo@stillwater-sc.com)
+
+#include <openkl/openkl_fwd.hpp>
+#include <openkl/mtl5_shim.hpp>
+
+
+namespace openkl
+{
+
+
+
+} // namespace openkl
--- tests/mat/write_dense_matrix_test.cpp
+++ tests/mat/write_dense_matrix_test.cpp
@@ -1,5 +1,5 @@
-// create_matrix_test.cpp
-// Created: 2020-03-25
+// write_dense_matrix_test.cpp
+// Created: 2020-03-26
 //
 // Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
 //
@@ -18,9 +18,6 @@
 #include <openkl/testing/check_object_info_output.hpp>
 #include <openkl/testing/check_object_content_output.hpp>
 
-#define MTL5_VERBOSE_TESTS // to see the output
-
-#include <mtl/mat/dense_matrix.hpp>
 #include <mtl/operations/io/test_ostream.hpp>
 #include <mtl/testing/check_equal.hpp>
 
@@ -33,15 +30,13 @@ int main()
 
     object_id a_id= create_dense_matrix<posit32>(2, 3);
     tout << object_info(a_id) << "\n";    
-    check_object_info_output(a_id, "matrix of dimension 2x3", "create_dense_matrix");
+    check_object_info_output(a_id, "matrix of dimension 2x3", "write_dense_matrix");
     tout << object_content(a_id) << "\n";
     
-    mtl::mat::dense_matrix<posit32> B{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}};
-    object_id b_id= create_dense_matrix<posit32>(2, 3, B[0][0]);
-    tout << object_info(b_id) << "\n";    
-    check_object_info_output(b_id, "matrix of dimension 2x3", "create_dense_matrix");
-    tout << object_content(b_id) << "\n";
-    check_object_content_output(b_id, "{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}}", "create_dense_matrix");
+//     mtl::mat::dense_matrix<posit32> A{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}};
+//     write_dense_matrix(a_id, A[0][0]);
+//     tout << object_content(b_id) << "\n";
+//     check_object_content_output(b_id, "{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}}", "write_dense_matrix");   
     
     return 0;
 }
