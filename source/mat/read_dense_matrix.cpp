c64e2fcf706fe9d6c756909fb5bce14c6d266e87
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-27 11:12:37 +0100

    read_dense_matrix done.

--- source/mat/read_dense_matrix.cpp
+++ source/mat/read_dense_matrix.cpp
@@ -1,5 +1,5 @@
-// write_dense_matrix.cpp
-// Created: 2020-03-26
+// read_dense_matrix.cpp
+// Created: 2020-03-27
 //
 // Copyright (C) 2020-present: Stillwater Supercomputing, Inc. & SimuNova UG
 //
@@ -10,30 +10,23 @@
 #include <openkl/openkl_fwd.hpp>
 #include <openkl/mtl5_shim.hpp>
 
-#include <openkl/utilities/object_id.hpp>
-#include <openkl/utilities/object_repo.hpp>
-
-#include <openkl/testing/check_presence.hpp>
-#include <openkl/testing/check_type.hpp>
-
+#include <openkl/utilities/get_object.hpp>
 #include <openkl/mat/dense_matrix.hpp>
-
 
 namespace openkl
 {
 
 template <typename Value>    
-void write_dense_matrix(object_id a_id, const Value& data)
+void read_dense_matrix(object_id A, Value& data)
 {
     using mtype= dense_matrix<Value>;
-    check_presence_debug( a_id ); 
-    check_type_debug<mtype>( a_id );
-    mtype* ap= static_cast<mtype*>(object_repo[a_id]);
-    ap->write(data);
+    mtype* ap= get_object<mtype>(A);
+    ap->read(data);
 }
 
 // explicit instantiations
-template void write_dense_matrix<posit32>(object_id, const posit32&);
+template void read_dense_matrix<posit32>(object_id, posit32&);
+
 
 
 } // namespace openkl
