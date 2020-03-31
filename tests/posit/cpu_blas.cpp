73b6cd9957984cb18d9e7d80674c59f0f4845b14
Peter Gottschling <peter.gottschling@simunova.com>
2020-03-31 10:51:49 +0200

    Some refactoring and uncommenting.

--- tests/posit/cpu_blas.cpp
+++ tests/posit/cpu_blas.cpp
@@ -7,36 +7,41 @@
 // Authors: Peter Gottschling (peter.gottschling@simunova.com)
 //          Theodore Omtzigt (theo@stillwater-sc.com)
 #include <iostream>
+#include <algorithm>
+
 #include <openkl/openkl.hpp>
+#include <openkl/utilities/exit.hpp>
 
 #include <universal/posit/posit>
 //#include <universal/posit/posit.hpp>
 
-std::string attributes(openkl::klComputeTarget& target) {
+std::string attributes(openkl::klExecutionEnvironment& target) {
 	std::stringstream ss;
+        // Most aren't implemented yet -- Peter
 	ss << "+-----\n"
-		<< " target             : " << target.id << '\n'
-		<< " compute class      : " << openkl::klComputeResourceTypeString[target.compute.resourceType] << '\n'
-		<< " compute identifier : " << target.compute.id << '\n'
-		<< " memory class       : " << openkl::klMemoryResourceTypeString[target.memory.resourceType] << '\n'
-		<< " memory size        : " << target.memory.size << " MBytes\n"
-		<< " memory channels    : " << target.memory.channels << '\n'
-		<< " memory page size   : " << target.memory.pageSize << " kBytes";
+		<< " target             : " << target.id << '\n'; 
+		// << " compute class      : " << openkl::klComputeResourceTypeString[target.compute.resourceType] << '\n'
+		// << " compute identifier : " << target.compute.id << '\n'
+		// << " memory class       : " << openkl::klMemoryResourceTypeString[target.memory.resourceType] << '\n'
+		// << " memory size        : " << target.memory.size << " MBytes\n"
+		// << " memory channels    : " << target.memory.channels << '\n'
+		// << " memory page size   : " << target.memory.pageSize << " kBytes";
 	return ss.str();
 }
 
 void ShowInventory(openkl::klComputeTargets& targets) {
 	std::cout << "\nInventory of compute resource targets\n";
-	for (int i = 0; i < targets.size(); ++i) {
-		std::cout << attributes(targets[i]) << std::endl;
-	}
+	for (auto& target : targets) 
+            std::cout << attributes(target) << std::endl;
 	std::cout << std::endl;
 }
 
 
 int main(int argc, char* argv[])
 try {
-	// first step: enumerate the target devices our program could use
+
+#if 0
+    // first step: enumerate the target devices our program could use
 	openkl::klComputeTargets targets;
 	openkl::klComputeTargetQuery query = openkl::klComputeTargetQuery{
 		openkl::klComputeResource{"",openkl::LOCAL_CPU},
@@ -60,7 +65,8 @@ try {
 
 	// second step: among the compute targets find a compute target matching your need
 	// we are going to look for a LOCAL_CPU target 
-	openkl::klComputeTarget target;
+	openkl::klExecutionEnvironment target;
+#if 0 // see below
 	target.compute.resourceType = openkl::COMPUTE_NOP;
 	for (int i = 0; i < targets.size(); ++i) {
 		if (targets[i].compute.resourceType == openkl::LOCAL_CPU) {
@@ -72,22 +78,28 @@ try {
 		std::cerr << "Unable to find a LOCAL_CPU compute target: exiting" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+#endif
+
+        // Same in (modern) STL style ;-)
+        auto is_cpu= [](auto& target){ return target.compute.resourceType == openkl::LOCAL_CPU; };
+        auto it= std::find_if(begin(targets), end(targets), is_cpu);
+        if (it == end(targets))
+            openkl::exit("Unable to find a LOCAL_CPU compute target: exiting");        
 
 	std::cout << "Successfully selected a LOCAL_CPU target\n";
 	std::cout << attributes(target) << std::endl;
 
 	// third step: create an execution context on the device of your choice
 	openkl::klComputeContext ctx;
-	if (!openkl::createContext(target, ctx)) {
-		std::cerr << "unable to create execution context on device " << target.id << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
+	if (!openkl::createContext(target, ctx))
+            openkl::exit("Unable to create execution context on device " + target.id);
+ 
 	// fourth step: allocate resources using the context
 
 
 	// fifth step: compute
-}
-catch(const char* msg) {
+#endif
+    
+} catch(const char* msg) {
 	std::cerr << "caught exception: " << msg << std::endl;
 }
